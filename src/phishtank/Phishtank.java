50fcc1a3f31672c95c60b1ba706b701b902c8c40
bar <bar@Bartsa>
2017-10-31 11:00:35 +0100

    modification4

---
+++ src/phishtank/Phishtank.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phishtank;
+
+/**
+ *
+ * @author bar
+ */
+public class Phishtank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        output>
+	<meta>
+		<total_entries>1234</total_entries>
+	</meta>
+	<entries>
+		<entry>
+			<url><![CDATA[https://www.example.com/]]></url>
+			<phish_id>123456</phish_id>
+			<phish_detail_url>http://www.phishtank.com/phish_detail.php?phish_id=123456</phish_detail_url>
+			<details>
+				<detail>
+					<ip_address>1.2.3.4</ip_address>
+					<cidr_block>1.2.3.0/24</ip_address>
+					<announcing_network>1234</announcing_network>
+					<rir>arin</rir>
+					<detail_time>2009-06-20T15:37:31+00:00</detail_time>
+				</detail>
+			</details>
+			<submission>
+				<submission_time>2009-06-19T15:15:47+00:00</submission_time>
+			</submission>
+			<verification>
+				<verified>yes</verified>
+				<verification_time>2009-06-19T15:37:31+00:00</verification_time>
+			</verification>
+			<status>
+				<online>yes</online>
+			</status>
+			<target>1st National Example Bank</target>
+		</entry>
+		...
+	</entries>
+</output>
+        // TODO code application logic here
+    }
+    
+}
